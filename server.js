906b99774b87d1df961f5748a4c4e8420c9f7adc
cddzx. <71639248+cddzx0705@users.noreply.github.com>
2026-03-14 23:35:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+const express = require("express");
+const app = express();
 app.get("/", (req,res)=>{
 res.send("SYSTEM IOS SERVER ONLINE");
 });
-const express = require("express");
-const app = express();
-
 app.use(express.json());
 
 /*
